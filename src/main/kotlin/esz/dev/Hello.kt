3bdc916d043bafb1334018043040724b9b65399c
Ernyoke <ernyoke19930115@gmail.com>
2019-01-22 02:15:00 +0200

    Hello.

--- src/main/kotlin/esz/dev/Hello.kt
+++ src/main/kotlin/esz/dev/Hello.kt
@@ -1,15 +1,5 @@
 package esz.dev
 
 fun main(args: Array<String>) {
-    val n = readLine()!!.toInt()
-    val values = readLine()?.split(" ")?.map { value: String ->
-        value.toInt()
-    }?.toMutableList() ?: mutableListOf()
-    val (res, multiSets) = esz.dev.codeforces.solve(values)
-    if (res) {
-        println("YES")
-        println(multiSets)
-    } else {
-        println("NO")
-    }
+    print("Hello")
 }
